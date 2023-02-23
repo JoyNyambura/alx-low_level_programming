dc463df232b0e04a060a50b756cd15b2d3ef716b
JoyNyambura <joyesthernyambura@gmail.com>
2023-02-23 13:20:28 -0800

    i_supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: variable text
+ *Return: always 0
+ */
+int_isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		Return(1);
+	else
+		Return(0);
+}
