4a0b813ad22275e45b6b466e4d5ebb49dbe9de50
JoyNyambura <joyesthernyambura@gmail.com>
2023-05-23 13:18:55 -0700

    Alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  *@c: char to be checked
  *Return: 1 if c is a letter , 0 if otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 	{
