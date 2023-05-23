fc22f999f87e7fe540507cd47bfe75e19277417f
JoyNyambura <joyesthernyambura@gmail.com>
2023-05-23 13:54:00 -0700

    Absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 /**
  * _abs - computes absolute value
  * @n: value to be checked
- * Return: absolute value 
+ * Return: absolute value
  */
 int _abs(int n)
 {
 	if (n >= 0)
 	{
-		return(n);
+		return (n);
 	}
 	else
 	{
