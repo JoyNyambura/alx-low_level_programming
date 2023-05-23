2efe3643869fe6bdf4f4bf2b4ddc5494280defcd
JoyNyambura <joyesthernyambura@gmail.com>
2023-05-23 14:08:53 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_last_digit - prints last digit
+ *@n: no with last digit
+ *Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = n % 10;
+	if ( ld <0)
+	{
+		_putchar(-ld + 48);
+		return (-ld);
+	}
+	else
+	{
+		_putchar(ld + 48);
+		return (ld);
+	}
+}
+
