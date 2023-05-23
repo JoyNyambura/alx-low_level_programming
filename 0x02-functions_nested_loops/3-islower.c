16611c441423722797fc1936dbf606f4b248c3ee
JoyNyambura <joyesthernyambura@gmail.com>
2023-05-23 12:44:54 -0700

    c-lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'Print c lowercase'
+ * Return: Always 0 (success)
+ */
+int _islower(int c)
+{
+	if (c = 'a'&& c; c <= 'z')
+	{
+		return (1):
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
