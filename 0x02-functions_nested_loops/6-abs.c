9901b1e289f744a4a7e5b246c32a45f018fb3dd4
JoyNyambura <joyesthernyambura@gmail.com>
2023-05-23 13:50:47 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value
+ * @n: value to be checked
+ * Return: absolute value 
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return(n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
