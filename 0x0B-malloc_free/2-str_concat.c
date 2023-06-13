7874c1a7b79e89638ddf80917a7ac7dce955d133
JoyNyambura <joyesthernyambura@gmail.com>
2023-06-13 16:42:50 -0700

    Grid

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		return (NULL);
+		return (" ");
 	}
 	if (s2 == NULL)
 	{
-		return (NULL);
+		return (" ");
 	}
 	i = j = 0;
 	while (s1[i] != '\0')
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *alloc_grind - allocate space for a 2D array
+ *@width: width of 2D array
+ *@height: height of 2D array
+ *
+ *Return: ptr to 2D array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int x, y;
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(int*) * height);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < height; x++)
+	{
+		grid[x] = malloc(sizeof(int) * width);
+	}
+	if (grid[x] == NULL)
+	{
+		return (NULL);
+	}
+	for (; x >= 0; x--)
+	{
+		free(grid[x]);
+			free(grid);
+			return (NULL);
+	}
+	for (x = 0; x < height; x++)
+	{
+		for(y = 0; y < width; y++)
+		{
+			grid[x][y] = 0;
+		}
+	}
+	return (grid);
+}
