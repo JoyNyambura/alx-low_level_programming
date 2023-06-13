035144ec7310ff08d935757f918def0c10f26db1
JoyNyambura <joyesthernyambura@gmail.com>
2023-06-13 15:55:53 -0700

    Concat

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	}
 
 	i = 0;
-	while (str != 0)
+	while (str[i] != '\0')
 	{
 		i++;
 	}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *str_concat - covatenates two strings
+ *@s1: string 1
+ *@s2: string 2
+ *
+ * Return: ptr to cocatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int i, j;
+
+	if (s1 == NULL)
+	{
+		return (NULL);
+	}
+	if (s2 == NULL)
+	{
+		return (NULL);
+	}
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	concat = malloc(sizeof(char) * (i + j + 1));
+
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		concat[i] = s2[j];
+		j++;
+	}
+	concat[i] = '\0';
+	return (concat);
+}
