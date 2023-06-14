8628b43ba6c2c8441f0bbe71075ec9da27bb7670
JoyNyambura <joyesthernyambura@gmail.com>
2023-06-14 13:37:49 -0700

    Concatenate

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *free_grid - Frees the space allocated to he grid
+ *@grid: 2D array
+ *@height: height
+ *Retru: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
+
