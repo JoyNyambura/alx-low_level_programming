c8178bc60f0f4ad2a844768075a64d5828c8c746
JoyNyambura <joyesthernyambura@gmail.com>
2023-06-14 13:53:08 -0700

    Grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 /**
- *alloc_grind - allocate space for a 2D array
+ *alloc_grid - allocate space for a 2D array
  *@width: width of 2D array
  *@height: height of 2D array
  *
@@ -12,32 +11,32 @@ int **alloc_grid(int width, int height)
 {
 	int **grid;
 	int x, y;
+
 	if (height <= 0 || width <= 0)
-	{
 		return (NULL);
-	}
-	grid = malloc(sizeof(int*) * height);
+
+	grid = malloc(sizeof(int *) * height);
+
 	if (grid == NULL)
-	{
 		return (NULL);
-	}
+
+
 	for (x = 0; x < height; x++)
 	{
 		grid[x] = malloc(sizeof(int) * width);
-	}
-	if (grid[x] == NULL)
-	{
-		return (NULL);
-	}
-	for (; x >= 0; x--)
-	{
-		free(grid[x]);
+
+		if (grid[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(grid[x]);
+
 			free(grid);
 			return (NULL);
+		}
 	}
 	for (x = 0; x < height; x++)
 	{
-		for(y = 0; y < width; y++)
+		for (y = 0; y < width; y++)
 		{
 			grid[x][y] = 0;
 		}
