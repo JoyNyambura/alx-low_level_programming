3b3b6f00668552161d1afd99225fe9bf59b5e0bb
JoyNyambura <joyesthernyambura@gmail.com>
2023-06-30 17:12:28 -0700

    Linked

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *print_list - prints all the data from the list
+ *@h: head pointer
+ *
+ *Return: no of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0](nil)\n");
+		else
+			printf("[%u]%s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
